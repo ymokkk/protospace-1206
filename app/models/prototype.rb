8b4389b444089daae9eef0ec1517a950b6a533da
ymokkk <yamasaan126@gmail.com>
2022-04-01 18:00:21 +0900

    バリデーションの設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,9 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :detail, presence: true
+  validates :point, presence: true
+  validates :image, presence: true
 end
