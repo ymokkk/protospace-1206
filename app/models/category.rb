039eec54f871528ec02b8ec33a8ab21a25841dc0
ymokkk <yamasaan126@gmail.com>
2022-04-04 14:10:40 +0900

    カテゴリーの追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,13 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
+    { id: 1, name: '--------' },
     { id: 2, name: 'スキンケア' },
     { id: 3, name: 'ボディケア' },
     { id: 4, name: '電子機器' },
     { id: 5, name: 'メイクアップ' },
     { id: 6, name: '本' },
-    { id: 7, name: 'バスグッズ' }
+    { id: 7, name: 'バスグッズ' },
+    { id: 8, name: 'その他'}
   ]
 
   include ActiveHash::Associations
